2b0e7441392e91b724ab700292b80aff8e66b295
HersonalREAL <jycdeya@gmail.com>
2020-12-24 01:40:19 +0800

    能爬第一页了

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -29,17 +29,33 @@ public class test {
         try {
 
             Document doc = Jsoup.connect(url).
-                    /*proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 1080))).*/get();
+                    proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 1080))).get();
 
-            Elements container = doc.getElementsByAttribute("title");
-
+            //根据key为id,value以line_u开头得到元素
+            Elements container = doc.getElementsByAttributeValueStarting("id","line_u");
+            //重新生成doc以便dom操作
             Document containerDoc = Jsoup.parse(container.toString());
+            //AllIwant!
+            Elements ALLIwant = containerDoc.select("a[href]");
 
-            String articleTitle = containerDoc.getElementsByAttribute("title").text();
+            //遍历爬取到的元素 进行处理
+            for(Element linkAndTitle:ALLIwant){
+                //生成地址
+                String theUrl = "http://news.gzhu.edu.cn/"+linkAndTitle.attr("href");
+                //得到标题
+                String theTitle = linkAndTitle.attr("title");
+                System.out.println("标题：" + theTitle);
+                //下载
+                source.download(theUrl,theTitle+".html");
+            }
+
+
+
+            //String articleTitle = containerDoc.getElementsByAttribute("title").text();
 //            String authorName = containerDoc.getElementById("authorName").text();
 //            String time = containerDoc.select("span").first().text();
 //            String imgphotoUrl=containerDoc.select("img").get(1).attr("src");
-            System.out.println("标题：" + articleTitle); //标题
+            //System.out.println("标题：" + articleTitle); //标题
 //            System.out.println("作者："+authorName); //作者
 //            System.out.println("发布时间："+time); //发布时间
 //            System.out.println("作者头像的url："+imgphotoUrl); //发布时间
